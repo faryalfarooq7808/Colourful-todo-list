0a0d12eb29b56a8d53923bc0c42b41f8734cc09e
faryalfarooq7808 <faryal.farooq780@gmail.com>
2024-04-03 18:46:14 -0700

    Adding a Colourfull ATM Machine

---
+++ main.js
@@ -0,0 +1,26 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todolist = [];
+let conditions = true;
+console.log(chalk.bgYellowBright.bold("\n \t Welcome to Faryal - Todo-List Application \n"));
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.bgCyan.bold("Enter your New Task :")
+        }
+    ]);
+    todolist.push(addTask.task);
+    console.log(`${addTask.task}Task added in Todo-List successfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.bgGreenBright.bold("Do you want to add more task ?"),
+            default: "False"
+        }
+    ]);
+    conditions = addMoreTask.addmore;
+}
+console.log("Your updated Todo-List:", todolist);
